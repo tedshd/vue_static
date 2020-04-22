24ff2f7dc1233f648b7ba1260ba884e7de4bb549
Ted Shu <ted.shu@ileopard.com>
2020-04-22 18:36:54 +0800

    [dev] Update webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,40 @@
-var path = require('path')
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const fs = require('fs')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const fs = require('fs')
+const { SitemapStream, streamToPromise } = require('sitemap')
 
-const { SitemapStream, streamToPromise } = require('sitemap');
-const siteMapUrls = [];
-const resolve = dir => path.join(__dirname, dir);
+const hostUrl = ''
+const routes = ['/', '/about/', '/contact/']
 
+const sitemapUrls = []
+const sitemapGenerate = []
+// const resolve = dir => path.join(__dirname, dir);
+
+// get i18n lang
 let langs = []
 fs.readdirSync(path.join(__dirname, 'src/locales')).forEach(file => {
   langs.push(file.replace('.json', ''))
 })
 
-const routes = ['/', '/about/', '/contact/']
+// generate i18n router for i18n page
 const langsPrerender = []
-const sitemapGenerate = []
+
 for (let index = 0; index < langs.length; index++) {
-  let lang = langs[index];
-  let langRoutes = Array.from(routes);
+  let lang = langs[index]
+  let langRoutes = Array.from(routes)
+
   for (let x = 0; x < langRoutes.length; x++) {
-    langRoutes[x] = '/' + lang + langRoutes[x];
+    langRoutes[x] = '/' + lang + langRoutes[x]
   }
+
+  langsPrerenderUpdate(lang, langRoutes)
+}
+
+function langsPrerenderUpdate (lang, langRoutes) {
   langsPrerender.push(
 
     new PrerenderSPAPlugin({
@@ -51,65 +62,49 @@ for (let index = 0; index < langs.length; index++) {
         injectProperty: '__PRERENDER_INJECTED__',
         inject: {
           isPrerender: true,
-          // 多语言输出
-          lang: lang
+          lang: lang // 多语言输出
         },
         headless: true,
-        renderAfterDocumentEvent: 'render-event'
+        renderAfterDocumentEvent: 'render-event' // 和 main.js 裡面的事件一致
       })
     })
   )
 }
 sitemapGenerate.push(
   new PrerenderSPAPlugin({
-    staticDir: resolve('dist'),
+    staticDir: path.join(__dirname, 'dist'),
     routes,
     postProcess (context) {
-      //content 參數
       const { originalRoute, route, html } = context;
-      //全局獲取href內容正則
       const reg = /(?<=<a\s*.*href=")[^"]*(?=")/g;
-      //過濾不包含http或https開頭的url
       const urlList = html.match(reg).filter(url => url.startsWith('http'));
-      //將路由添加到全局sitemap容器
-      siteMapUrls.push(originalRoute);
-      //將html中的外鏈添加到全局sitemap容器
+      sitemapUrls.push(originalRoute);
       if (urlList.length) {
-        urlList.forEach(url => siteMapUrls.push(url));
+        urlList.forEach(url => sitemapUrls.push(url));
       }
-      //噹噹前路由為最後一個生成路由時
       if (route === routes[routes.length - 1]) {
-        //去除重複的鏈接
-        let currentSiteMapUrls = Array.from(new Set(siteMapUrls));
-        //過濾掉鏈接中的錨點後內容
-        currentSiteMapUrls = currentSiteMapUrls.map(url => {
+        let currentSitemapUrls = Array.from(new Set(sitemapUrls));
+        currentSitemapUrls = currentSitemapUrls.map(url => {
           const isMao = url.indexOf('#') > -1;
-          //生成sitemap所需數據，具體參數參詳sitemap.js官網
-          return { url: isMao ? url.split('#')[0] : url, changefreq: 'weekly', priority: 0.6, lastmod: new Date().toLocaleDateString() }
+          return { url: isMao ? url.split('#')[0] : url, changefreq: 'weekly', priority: 0.1, lastmod: new Date().toLocaleDateString() }
         });
-        console.log(currentSiteMapUrls)
+        console.log(currentSitemapUrls)
 
         const smStream = new SitemapStream({
           hostname: 'http://www.mywebsite.com',
-          // xslUrl: "https://example.com/style.xsl",
           lastmodDateOnly: false, // print date not time
         })
-        // coalesce stream to value
-        // alternatively you can pipe to another stream
 
-        for (let index = 0; index < currentSiteMapUrls.length; index++) {
-          smStream.write(currentSiteMapUrls[index]);
+        for (let index = 0; index < currentSitemapUrls.length; index++) {
+          smStream.write(currentSitemapUrls[index]);
         }
         smStream.end()
 
         streamToPromise(smStream).then(function (xml) {
           console.log(xml.toString())
-          fs.writeFileSync(resolve('dist/sitemap.xml'), xml.toString());
+          fs.writeFileSync(path.join(__dirname, 'dist/sitemap.xml'), xml.toString());
         })
-        //生成siteMap文件
       }
-      //返回當前contet對象
-
       return context
     },
     renderer: new Renderer({
@@ -157,7 +152,12 @@ module.exports = {
         test: /\.css$/,
         use: [
           'vue-style-loader',
-          'css-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              // minimize: true
+            }
+          },
           {
             loader: 'postcss-loader',
             options: {
@@ -165,9 +165,9 @@ module.exports = {
               plugins: (loader) => [
                 require('postcss-import')(),
                 require('postcss-apply')(),
-                require('postcss-css-variables')()
+                require('postcss-css-variables')(),
+                require('cssnano')()
                 // require('postcss-preset-env')(),
-                // require('cssnano')()
               ]
             }
           }
@@ -205,7 +205,7 @@ if (process.env.NODE_ENV === 'production') {
         favicon: 'favicon.ico'
       })
     ])
-    // .concat(langsPrerender)
+    .concat(langsPrerender)
     .concat(sitemapGenerate)
 
 } else {
